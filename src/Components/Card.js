d34276e555f7bb642d3bb97840091481de0d834f
KingShahrukh <srks50940@gmail.com>
2024-04-08 00:12:17 +0530

    my change

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -13,22 +13,28 @@ import {
 } from "mdb-react-ui-kit";
 import { useDispatch, useSelector } from "react-redux";
 import { addtocart } from "../features/cartSlice";
+
 export default function App() {
   const items = useSelector((state) => state.allcart.items);
   const dispatch = useDispatch();
+
   return (
     <MDBContainer>
-      <MDBRow className="mb-3">
+      <MDBRow className="row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         {(items || []).map((item) => (
-          <MDBCol size="4" key={item.Id}>
-            <MDBCard className="mt-3" id="card">
+          <MDBCol key={item.Id}>
+            <MDBCard className="mt-3 h-100" id="card">
               <MDBRipple
                 rippleColor="light"
                 rippleTag="div"
                 className="bg-image hover-overlay"
               >
-                <MDBCardImage src={item.Img} className='img-fluid shadow-4' alt="iphone" />
-                <a>
+                <MDBCardImage
+                  src={item.Img}
+                  className="img-fluid shadow-4"
+                  alt="iphone"
+                />
+                <a href="/">
                   <div
                     className="mask"
                     style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
@@ -39,14 +45,13 @@ export default function App() {
                 <MDBCardTitle>{item.Title}</MDBCardTitle>
                 <MDBCardText>{item.Description}</MDBCardText>
                 <h5> Price: {item.PRICE}₹</h5>
-                <MDBBtn href="#" onClick={() => dispatch(addtocart(item))}>
+                <MDBBtn onClick={() => dispatch(addtocart(item))}>
                   Add to Cart
                 </MDBBtn>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
         ))}
-        
       </MDBRow>
     </MDBContainer>
   );
--- src/Components/Productcart.js
+++ src/Components/Productcart.js
@@ -11,30 +11,30 @@ const Productcart = () => {
 
   return (
     <div>
-      <section class="h-100 gradient-custom">
-        <div class="container py-5">
-          <div class="row d-flex justify-content-center my-4">
-            <div class="col-md-8">
-              <div class="card mb-4">
-                <div class="card-header py-3">
-                  <h5 class="mb-0">Total-Items:{cart.length}</h5>
+      <section ClassName="h-100 gradient-custom">
+        <div ClassName="container py-5">
+          <div ClassName="row d-flex justify-content-center my-4">
+            <div ClassName="col-md-8">
+              <div ClassName="card mb-4">
+                <div ClassName="card-header py-3">
+                  <h5 ClassName="mb-0">Total-Items:{cart.length}</h5>
                 </div>
-                <div class="card-body">
+                <div ClassName="card-body">
                   {(cart || []).map((data) => (
-                    <div class="row">
-                      <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                    <div ClassName="row">
+                      <div ClassName="col-lg-3 col-md-12 mb-4 mb-lg-0">
                         <div
-                          class="bg-image hover-overlay hover-zoom ripple rounded"
+                          ClassName="bg-image hover-overlay hover-zoom ripple rounded"
                           data-mdb-ripple-color="light"
                         >
                           <img
                             src={data.Img}
-                            class="w-100"
+                            ClassName="w-100"
                             alt="Blue Jeans Jacket"
                           />
                           <a href="#!">
                             <div
-                              class="mask"
+                              ClassName="mask"
                               style={{
                                 backgroundColor: "rgba(251, 251, 251, 0.2)",
                               }}
@@ -43,7 +43,7 @@ const Productcart = () => {
                         </div>
                       </div>
 
-                      <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                      <div ClassName="col-lg-5 col-md-6 mb-4 mb-lg-0">
                         <p>
                           <strong>{data.Title}</strong>
                         </p>
@@ -51,73 +51,73 @@ const Productcart = () => {
 
                         <button
                           type="button"
-                          class="btn btn-primary btn-sm me-1 mb-2"
+                          ClassName="btn btn-primary btn-sm me-1 mb-2"
                           data-mdb-toggle="tooltip"
                           title="Remove item"
                           onClick={() => dispatch(removeFromCart(data.Id))}
                         >
-                          <i class="fas fa-trash"></i>
+                          <i ClassName="fas fa-trash"></i>
                         </button>
                       </div>
 
-                      <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                        <div class="d-flex mb-4" style={{ maxWidth: "300px" }}>
+                      <div ClassName="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                        <div ClassName="d-flex mb-4" style={{ maxWidth: "300px" }}>
                           <button
-                            class="btn btn-primary px-3 me-2"
+                            ClassName="btn btn-primary px-3 me-2"
                             onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
                             onClick={() => dispatch(decreament(data.Id))}
 
                           >
-                            <i class="fas fa-minus"></i>
+                            <i ClassName="fas fa-minus"></i>
                           </button>
 
-                          <div class="form-outline">
+                          <div ClassName="form-outline">
                             <input
                               id="form1"
                               min="0"
                               name="quantity"
                               value={data.Quantity}
                               type="number"
-                              class="form-control"
+                              ClassName="form-control"
                             />
-                            <label class="form-label" for="form1">
+                            <label ClassName="form-label" for="form1">
                               Quantity
                             </label>
                           </div>
 
                           <button
-                            class="btn btn-primary px-3 ms-2"
+                            ClassName="btn btn-primary px-3 ms-2"
                             onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
                             onClick={() => dispatch(increament(data.Id))}
 >
-                            <i class="fas fa-plus"></i>
+                            <i ClassName="fas fa-plus"></i>
                           </button>
                         </div>
 
-                        <p class="text-start text-md-center">
+                        <p ClassName="text-start text-md-center">
                           <strong>{data.PRICE}₹</strong>
                         </p>
                       </div>
                     </div>
                   ))}
 
-                  <hr class="my-4" />
+                  <hr ClassName="my-4" />
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="card mb-4">
-                <div class="card-header py-3">
-                  <h5 class="mb-0">Summary</h5>
+            <div ClassName="col-md-4">
+              <div ClassName="card mb-4">
+                <div ClassName="card-header py-3">
+                  <h5 ClassName="mb-0">Summary</h5>
                 </div>
-                <div class="card-body">
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                <div ClassName="card-body">
+                  <ul ClassName="list-group list-group-flush">
+                    <li ClassName="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
                       TotalQuantity
                       <span>{totalQuantity}</span>
                     </li>
 
-                    <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                    <li ClassName="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                       <div>
                         <strong>Total amount</strong>
                       </div>
@@ -129,7 +129,7 @@ const Productcart = () => {
 
                   <button
                     type="button"
-                    class="btn btn-primary btn-lg btn-block"
+                    ClassName="btn btn-primary btn-lg btn-block"
                   >
                     Go to checkout
                   </button>
